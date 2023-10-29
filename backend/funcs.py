a5c948a223a2dd04f323ea1a4756dd8cc2c917d8
Rink <bystrova1979inbox.ru@gmail.com>
2023-10-29 18:55:05 +0300

    Made to the end other functions

--- backend/funcs.py
+++ backend/funcs.py
@@ -1,32 +1,46 @@
-import flask
 from flask import request, jsonify, Flask
 
 import db
 import visualizator
 from conv import time2str, date2str
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 cur = None
 conn = None
 
-# убрать # из апп роута, условие сделать нормальным
 
-def db_exec(what):
-    """
-    Функция, в которую поставляется строка, которая будет исполнена в Query Sql-я
-    Прекрасно понимаю, что это небезопасно,
-    но если что-то вдруг понадобится быстро сделать - неплохой вариант пока что
-    """
-    cur.execute(what)
+@app.route('/meeting_room', methods=['GET'])
+def get_meeting_rooms():
+    cur.execute(f"SELECT id, name, description FROM meeting_rooms")
+    fetch = cur.fetchall()
+    meeting_rooms = []
+    for x in fetch:
+        meeting_room = {
+            "id": x[0],
+            "name": x[1],
+            "description": x[2],
+        }
+        meeting_rooms.append(meeting_room)
+    return jsonify(meeting_rooms)
 
 @app.route('/meeting_room/<id>', methods=['GET'])
 def get_meeting_room(id):
-    return jsonify({"message": f"Hello, {id}"})
+    cur.execute(f"SELECT id, name, description, time FROM meeting_rooms WHERE id={id}")
+    fetch = cur.fetchone()
+    if fetch == None:
+        return jsonify({"message": f"Переговорки с id {id} не было найдено"}), 404
+    meeting_room = {
+        "id": fetch[0],
+        "name": fetch[1],
+        "description": fetch[2],
+        "time": fetch[3],
+    }
+    return jsonify(meeting_room)
 
 @app.route('/meeting_room/<id>', methods=['POST'])
-def take_meeting_room_time(id):
+def post_meeting_room_time(id):
     data = request.json
     cur.execute(f"SELECT time FROM meeting_rooms WHERE id={id}")
     time = cur.fetchone()[0]
@@ -37,21 +51,29 @@ def take_meeting_room_time(id):
         return data, 201
     else:
         return jsonify({"message": "Это время уже занято"}), 404
+
+@app.route('/meeting_room/<id>', methods=['DELETE'])
+def delete_meeting_room_time(id):
+    data = request.json
+    cur.execute(f"SELECT time FROM meeting_rooms WHERE id={id}")
+    time = cur.fetchone()[0]
+    for t in data["time"]:
+        try:
+            time.remove(t)
+        except ValueError:
+            pass
+    if len(time) > 0:
+        cur.execute(f"UPDATE meeting_rooms SET time = ARRAY {time} WHERE id={id}")
+    else: # По какой-то причине я не могу просто дать пустой массив, требует тип массива, который я не знаю как указать
+        # Посему приходится вводить этот if-else
+        cur.execute(f"UPDATE meeting_rooms SET time = ARRAY[]::integer[] WHERE id={id}")
+    return data, 200
+
 @app.route('/v', methods=['GET'])
 def visualize_please():
     global cur
     visualizator.visualize_tables(cur)
     return jsonify({"message": "visualized"}), 200
-
-@app.route('/stationary', methods=['POST'])
-def post_stationary_problem():
-    """
-    json должен иметь в себе user_id, type, amount, priority
-    """
-    if request.method == 'POST':
-        req = request.get_json()
-        cur.execute(
-            f"""INSERT INTO stationary_problem (created_at, updated_at, user_id, type, amount, priority) VALUES (NOW(), NOW(), {req[0]}, {req[1]}, {req[2]}, {req[3]}, )""")
 
 if __name__ == "__main__":
     cur, conn = db.connect()
